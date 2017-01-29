5bc1ea6886705f810f677d8ad0dddc2af58935e0
Caleb Faught <faughtcj@gmail.com>
2017-01-29 17:51:59 -0600

    Added pagination for log selection menu

--- app/assets/javascripts/dives/dives.controller.js
+++ app/assets/javascripts/dives/dives.controller.js
@@ -4,18 +4,46 @@
 
   function DivesController (DivesService, $stateParams, $scope, $state) {
     var vm = this;
+    $scope.allDives;
+    vm.dives;
 
     vm.createDive = createDive;
     vm.deleteDive = deleteDive;
 
+    var PAGE_LENGTH = 4;
+
+    vm.page = 0;
+    vm.totalDives;
+    vm.totalPages;
+
+    $scope.paginate = function () {
+      vm.dives = $scope.allDives.slice(vm.page * PAGE_LENGTH, (vm.page + 1) * PAGE_LENGTH);
+    }
+
+    vm.nextPage = function () {
+      vm.page += 1;
+      $scope.paginate();
+    }
+
+    vm.lastPage = function () {
+      vm.page -= 1;
+      $scope.paginate();
+    }
+
     DivesService.all()
       .then(function(data) {
-        $scope.dives = data;
+        $scope.allDives = data;
+        vm.totalDives = $scope.allDives.length;
+        vm.totalPages = Math.ceil(vm.totalDives / PAGE_LENGTH);
+        $scope.paginate();
       }).catch(function(err) {
         err.message;
       })
 
+
+
     if ($stateParams.id) {
+
       DivesService.getDetail($stateParams.id)
         .then(function(data) {
           vm.dive = data;
@@ -25,9 +53,10 @@
     function createDive() {
       DivesService.create(vm.dive)
         .then(function(dive) {
-          $scope.$parent.dives.push(dive)
+          $scope.$parent.allDives.push(dive)
           vm.dive = dive
           $state.go('dives.detail', dive)
+          $scope.$parent.paginate();
         })
         .then(function(dive) {
           vm.dive = {}
@@ -38,10 +67,11 @@
       DivesService
         .deleteDive(vm.dive.id)
         .then(function() {
-          var currentDives = $scope.$parent.dives.filter(function(dive) {
+          var currentDives = $scope.$parent.allDives.filter(function(dive) {
             return dive.id !== vm.dive.id
           });
-          $scope.$parent.dives = currentDives;
+          $scope.$parent.allDives = currentDives;
+          $scope.$parent.paginate();
         })
         .then(function() {
           $state.go('dives')
